a5585af6594c8922ba5028c8bad39535f54f54dd
viinnoo <vinokazu_firmansyah_33rpl@student.smktelkom-mlg.sch.id>
2026-05-31 10:57:21 +0700

    ganti jadi swagger dari api

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document, {
+  SwaggerModule.setup('swagger', app, document, {
     customSiteTitle: 'UKL Sama Abebeye API Docs',
   });
 
